d000cc0d0237812a634cfc5d0105a3594f76a200
zkhdev <zkh-101@outlook.com>
2017-05-29 14:45:29 +0800

    添加图片裁剪功能

--- src/components/Upload/Upload.vue
+++ src/components/Upload/Upload.vue
@@ -22,12 +22,19 @@
         <el-row :gutter="10">
             <el-col :span="12">
                 <div class="card upload-card">
-                    <v-avatar></v-avatar>
+                    <v-drag></v-drag>
                 </div>
             </el-col>
             <el-col :span="12">
                 <div class="card upload-card">
-                    <v-drag></v-drag>
+                    <v-avatar></v-avatar>
+                </div>
+            </el-col>
+        </el-row>
+        <el-row :gutter="10">
+            <el-col :span="12">
+                <div class="card upload-card">
+                    <v-crop></v-crop>
                 </div>
             </el-col>
         </el-row>
@@ -39,12 +46,14 @@ import Batch from './Batch.vue'
 import Drag from './Drag.vue'
 import QiniuSdk from './QiniuSdk.vue'
 import Avatar from './Avatar.vue'
+import Crop from './Crop.vue'
 export default {
     components: {
         'v-batch': Batch,
         'v-drag': Drag,
         'v-qiniusdk': QiniuSdk,
-        'v-avatar': Avatar
+        'v-avatar': Avatar,
+        'v-crop': Crop
     }
 }
 </script>
